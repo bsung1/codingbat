c4d31da3eab5b259ac02f7f02bdd1831c552de62
bsung1 <70121996+bsung1@users.noreply.github.com>
2020-09-11 10:25:07 -0400

    practice

---
+++ warmup-1/monkey_trouble.py
@@ -0,0 +1,6 @@
+def monkey_trouble(a_smile, b_smile):
+    """are we in trouble (True)"""
+    if a_smile == b_smile:
+        return True
+    else:
+        return False
---
+++ warmup-1/sum_double.py
@@ -0,0 +1,5 @@
+def sum_double(a, b):
+    if a == b:
+        return 4 * a
+    if a != b:
+        return a + b
